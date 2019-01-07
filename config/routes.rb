36f3f064555672bbfac8a91afc213b3f03f75b51
Douglas Bay <douglasbays@gmail.com>
2019-01-07 17:32:27 -0500

    Rails Basics

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,16 @@
 Rails.application.routes.draw do
+
+  get 'about', to: 'pages#about'
+
+  get 'contact', to: 'pages#contact'
+
+  # http://localhost:3000/restaurants
+  get 'restaurants', to: 'restaurants#index'
+
+  post 'restaurants', to: 'restaurants#create'
+
+  get 'restaurants/:id', to: 'restaurants#show', as: :restaurant
+
+  root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
